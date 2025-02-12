99974a659da8aae0dc01bb1d35804154dd0153a6
Nathan Adler <adlerbn@rose-hulman.edu>
2025-02-11 20:27:04 -0500

    Addded proc fields

--- xv6-riscv/kernel/proc.h
+++ xv6-riscv/kernel/proc.h
@@ -91,7 +91,7 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
-
+  
   // wait_lock must be held when using this:
   struct proc *parent;         // Parent process
 
@@ -104,4 +104,9 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  
+  // for threads
+  int is_thread;              //If non-zero, is a thread
+  int tid;                    //If non-zero, the id of the thread when using thread api
+  void* stack;                //A pointer to the private stack of the thread
 };
